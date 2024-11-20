299b3c41b569f608d1ef9b5a31b47cd3f1805ab2
Eduardo Leon Arvelo <alu0101475615@ull.edu.es>
2024-11-20 23:13:10 +0000

    Update ParserTrainer.java

--- src/main/java/org/fogbeam/example/opennlp/training/ParserTrainer.java
+++ src/main/java/org/fogbeam/example/opennlp/training/ParserTrainer.java
@@ -1,13 +1,12 @@
 package org.fogbeam.example.opennlp.training;
 
-public class ParserTrainer
-{
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	public static void main( String[] args )
-	{
-		
-		
-		
-		System.out.println( "done" );
-	}
+public class ParserTrainer {
+    private static final Logger logger = LoggerFactory.getLogger(ParserTrainer.class);
+
+    public static void main(String[] args) {
+        logger.info("Processing complete.");
+    }
 }
