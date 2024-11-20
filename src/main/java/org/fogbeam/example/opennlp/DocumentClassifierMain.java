1e71b13ca99a8febaa7f75d39455a06a77763c3f
Eduardo Leon Arvelo <alu0101475615@ull.edu.es>
2024-11-20 22:45:19 +0000

    Update DocumentClassifierMain.java

--- src/main/java/org/fogbeam/example/opennlp/DocumentClassifierMain.java
+++ src/main/java/org/fogbeam/example/opennlp/DocumentClassifierMain.java
@@ -3,45 +3,50 @@ package org.fogbeam.example.opennlp;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import opennlp.tools.doccat.DoccatModel;
 import opennlp.tools.doccat.DocumentCategorizerME;
 
 public class DocumentClassifierMain
 {
+    private static final Logger logger = LoggerFactory.getLogger(DocumentClassifierMain.class);
 
-	public static void main( String[] args ) throws Exception
-	{
-		
-		InputStream is = null;
-		try
-		{
-			is = new FileInputStream( "models/en-doccat.model" );
-		
-			DoccatModel m = new DoccatModel(is);
-			
-			
-			String inputText = "What happens if we have declining bottom-line revenue?";
-			DocumentCategorizerME myCategorizer = new DocumentCategorizerME(m);
-			double[] outcomes = myCategorizer.categorize(inputText);
-			String category = myCategorizer.getBestCategory( outcomes );
-			
-			System.out.println( "Input classified as: " + category );
-			
-			
-		}
-		catch( Exception e )
-		{
-			e.printStackTrace();
-			
-		}
-		finally
-		{
-			if( is != null )
-			{
-				is.close();
-			}
-		}
-		
-		System.out.println( "done" );
-	}
+    public static void main(String[] args) throws Exception
+    {
+        InputStream is = null;
+        try
+        {
+            is = new FileInputStream("models/en-doccat.model");
+            DoccatModel m = new DoccatModel(is);
+
+            String inputText = "What happens if we have declining bottom-line revenue?";
+            DocumentCategorizerME myCategorizer = new DocumentCategorizerME(m);
+            double[] outcomes = myCategorizer.categorize(inputText);
+            String category = myCategorizer.getBestCategory(outcomes);
+
+            logger.info("Input classified as: {}", category);
+        }
+        catch (Exception e)
+        {
+            logger.error("An error occurred during document classification", e);
+        }
+        finally
+        {
+            if (is != null)
+            {
+                try
+                {
+                    is.close();
+                }
+                catch (Exception e)
+                {
+                    logger.warn("Error closing the input stream", e);
+                }
+            }
+        }
+
+        logger.info("Processing complete");
+    }
 }
