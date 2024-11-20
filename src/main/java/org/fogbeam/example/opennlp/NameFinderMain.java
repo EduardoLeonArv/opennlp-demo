e0404a5b979579fb2ed57dc7f065e6bf5cd7dc85
Eduardo Leon Arvelo <alu0101475615@ull.edu.es>
2024-11-20 23:27:21 +0000

    Vuelta a versiones antiguas

--- src/main/java/org/fogbeam/example/opennlp/NameFinderMain.java
+++ src/main/java/org/fogbeam/example/opennlp/NameFinderMain.java
@@ -1,72 +1,86 @@
+
 package org.fogbeam.example.opennlp;
+
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
 import opennlp.tools.util.Span;
 
+
 public class NameFinderMain
 {
-    private static final Logger logger = LoggerFactory.getLogger(NameFinderMain.class);
-
-    public static void main(String[] args) throws Exception
-    {
-        InputStream modelIn = null;
-        try
-        {
-            modelIn = new FileInputStream("models/en-ner-person.model");
-            TokenNameFinderModel model = new TokenNameFinderModel(modelIn);
-            NameFinderME nameFinder = new NameFinderME(model);
-
-            String[] tokens = { 
-                "Phillip", 
-                "Rhodes",
-                "is",
-                "presenting",
-                "at",
-                "some",
-                "meeting",
-                "."
-            };
-
-            Span[] names = nameFinder.find(tokens);
-
-            if (logger.isInfoEnabled()) {
-                for (Span ns : names)
-                {
-                    logger.info("Name span found: {}", ns); // No se llama explícitamente a toString()
-                }
-            }
-
-            nameFinder.clearAdaptiveData();
-        }
-        catch (IOException e)
-        {
-            logger.error("Error occurred while processing the model or finding names", e);
-        }
-        finally
-        {
-            if (modelIn != null)
-            {
-                try
-                {
-                    modelIn.close();
-                }
-                catch (IOException e)
-                {
-                    logger.warn("Error closing the model input stream", e);
-                }
-            }
-        }
-
-        if (logger.isInfoEnabled()) {
-            logger.info("Processing complete");
-        }
-    }
+	/**
+	 * @param args
+	 */
+	public static void main( String[] args ) throws Exception
+	{
+		InputStream modelIn = new FileInputStream( "models/en-ner-person.model" );
+		// InputStream modelIn = new FileInputStream( "models/en-ner-person.bin" );
+		
+		try
+		{
+			TokenNameFinderModel model = new TokenNameFinderModel( modelIn );
+		
+			NameFinderME nameFinder = new NameFinderME(model);
+			
+			String[] tokens = { //"A", "guy", "named",
+								// "Mr.", 
+								"Phillip", 
+								"Rhodes",
+								"is",
+								"presenting",
+								"at",
+								"some",
+								"meeting",
+								"."};
+			
+			Span[] names = nameFinder.find( tokens );
+		
+			for( Span ns : names )
+			{
+				System.out.println( "ns: " + ns.toString() );
+			
+				// if you want to actually do something with the name
+				// ...
+				
+			}
+		
+			nameFinder.clearAdaptiveData();
+			
+		}
+		catch( IOException e )
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if( modelIn != null )
+			{
+				try
+				{
+					modelIn.close();
+				}
+				catch( IOException e )
+				{
+				}
+			}
+		}
+		
+		
+		System.out.println( "done" );
+	}
 }
+
+/* 				
+				StringBuilder sb = new StringBuilder();
+				for( int i = ns.getStart(); i < ns.getEnd(); i++ )
+				{
+					sb.append( tokens[i] + " " );
+				}
+				
+				System.out.println( "The name is: " + sb.toString() );
+ */
