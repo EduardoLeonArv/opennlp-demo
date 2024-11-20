d998eb171a3948d36fd7d706ff3ae15413e7a91e
Eduardo Leon Arvelo <alu0101475615@ull.edu.es>
2024-11-20 23:01:00 +0000

    Update PartOfSpeechTaggerMain.java

--- src/main/java/org/fogbeam/example/opennlp/PartOfSpeechTaggerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/PartOfSpeechTaggerMain.java
@@ -1,68 +1,63 @@
-
 package org.fogbeam.example.opennlp;
-
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSTaggerME;
 
-
 public class PartOfSpeechTaggerMain
 {
-	public static void main( String[] args )
-	{
-		InputStream modelIn = null;
-		try
-		{
-			// modelIn = new FileInputStream( "models/en-pos.model" );
-			modelIn = new FileInputStream( "models/en-pos-maxent.bin" );
-			
-			POSModel model = new POSModel( modelIn );
-			
-			
-			POSTaggerME tagger = new POSTaggerME(model);
-			
-			String sent[] = new String[]{"Most", "large", "cities", "in", "the", "US", "had",
-                    "morning", "and", "afternoon", "newspapers", "."};		  
-			String tags[] = tagger.tag(sent);
-			
-			double probs[] = tagger.probs();
-			
-			
-			for( int i = 0; i < sent.length; i++ )
-			{
-				System.out.println( "Token [" + sent[i] + "] has POS [" + tags[i] + "] with probability = " + probs[i] );
-			}
-			
-			
-		}
-		catch( IOException e )
-		{
-			// Model loading failed, handle the error
-			e.printStackTrace();
-		}
-		finally
-		{
-			if( modelIn != null )
-			{
-				try
-				{
-					modelIn.close();
-				}
-				catch( IOException e )
-				{
-				}
-			}
-		}
+    private static final Logger logger = LoggerFactory.getLogger(PartOfSpeechTaggerMain.class);
 
-		
+    public static void main(String[] args)
+    {
+        InputStream modelIn = null;
+        try
+        {
+            modelIn = new FileInputStream("models/en-pos-maxent.bin");
+            POSModel model = new POSModel(modelIn);
 
-		
-		
-		
-		System.out.println( "done" );
-	}
+            POSTaggerME tagger = new POSTaggerME(model);
+
+            String[] sent = new String[]{
+                "Most", "large", "cities", "in", "the", "US", "had",
+                "morning", "and", "afternoon", "newspapers", "."
+            };
+            String[] tags = tagger.tag(sent);
+            double[] probs = tagger.probs();
+
+            for (int i = 0; i < sent.length; i++) {
+                if (logger.isInfoEnabled()) {
+                    logger.info("Token [{}] has POS [{}] with probability = {}", sent[i], tags[i], probs[i]);
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            logger.error("Model loading failed or error during processing", e);
+        }
+        finally
+        {
+            if (modelIn != null)
+            {
+                try
+                {
+                    modelIn.close();
+                }
+                catch (IOException e)
+                {
+                    logger.warn("Error closing the model input stream", e);
+                }
+            }
+        }
+
+        if (logger.isInfoEnabled()) {
+            logger.info("Processing complete");
+        }
+    }
 }
